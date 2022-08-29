d48fcd4b264f18912f7838ff0ced5210a51dc0bb
SnAlex555 <desert_eagle666@mail.ru>
2022-08-29 22:03:49 +0300

    last commit

--- js/components/swiper1.js
+++ js/components/swiper1.js
@@ -1,7 +1,32 @@
 var swiper = new Swiper('.swiper1', {
   direction:'horizontal',
-  slidesPerView: 4.5,
   spaceBetween: 0,
+  breakpoints:{
+    1600:{
+      slidesPerView:3.5,
+      
+    },
+    1400:{
+      slidesPerView:3,
+    },
+    1150:{
+      slidesPerView:2.5,
+    },
+    // 768:{
+    //   slidesPerView:
+    // },
+    500:{
+      slidesPerView:2,
+    },
+    500:{
+      slidesPerView:1.5,
+    },
+    320:{
+      slidesPerView:1,
+    }
+  
+
+  } ,
   loop: true,
 
   pagination: {
@@ -13,4 +38,6 @@ var swiper = new Swiper('.swiper1', {
     nextEl: '.swiper-button-next1',
     prevEl: '.swiper-button-prev1',
   },
+
+  
 });
